a1eb42811b667ac7adeb7c67ef02aea4b0a10c5a
Iakiv Kramarenko <yashaka@gmail.com>
2016-02-09 20:16:49 +0200

    added docstring for tools.visit

--- selene/tools.py
+++ selene/tools.py
@@ -7,11 +7,25 @@ def set_driver(driver):
 
 
 def get_driver():
-    return config.driver
+    return config.drive
 
 
-def visit(relative_url):
-    get_driver().get(config.app_host + relative_url)
+def visit(absolute_or_relative_url):
+    """
+    Loads a web page in the current browser session.
+    :param absolute_or_relative_url:
+        an absolute url to web page in case of config.app_host is not specified,
+        otherwise - relative url correspondingly
+
+    :Usage:
+        visit('http://mydomain.com/subpage1')
+        visit('http://mydomain.com/subpage2')
+        # OR
+        config.app_host = 'http://mydomain.com'
+        visit('/subpage1')
+        visit('/subpage2')
+    """
+    get_driver().get(config.app_host + absolute_or_relative_url)
 
 
 def s(css_selector_or_locator):
