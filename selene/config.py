5a0927e820030d8520746e1a5d55ec64e170ed25
Iakiv Kramarenko <yashaka@gmail.com>
2016-02-22 13:48:18 +0200

    small updates

--- selene/config.py
+++ selene/config.py
@@ -1,8 +1,8 @@
-driver = None
+driver = None  # todo: make it "private" and move somewhere outside of public config
 timeout = 4
 poll_during_waits = 0.1
 app_host = ''
 
 # To cash all elements after first successful find
 #   config.cash_elements = True
-cash_elements = False # todo: test the True value :)
+cash_elements = False  # todo: test the True value :)
