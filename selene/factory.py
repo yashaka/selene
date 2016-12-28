75ef4465ba2fd362fa81024e6dbc57ec191430ea
sepi <sepi@ciklum.com>
2016-12-28 12:21:01 +0200

    driver factory import error fix

--- selene/factory.py
+++ selene/factory.py
@@ -4,24 +4,24 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
+import selene
 from selene.browsers import Browser
-from selene.tools import get_driver, set_driver
 
 
 def is_driver_initialized(name):
     try:
-        return get_driver().name == name
+        return selene.tools.get_driver().name == name
     except AttributeError:
         return False
 
 
 def start_browser(name):
     if is_driver_initialized(name):
-        return get_driver()
+        return selene.tools.get_driver()
 
     if name == Browser.CHROME:
         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
     else:
         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     atexit.register(driver.quit)
-    set_driver(driver)
+    selene.tools.set_driver(driver)
