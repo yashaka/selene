1854465a8547ca4fc97a20ad60ee39bdcaabf026
SergeyPirogov <Semen4ik20>
2017-01-22 19:16:25 +0200

    python 3 support
    fixed factory. added check for NoneObject

--- tests/integration/auto_start_test.py
+++ tests/integration/auto_start_test.py
@@ -11,12 +11,12 @@ def test_manual_start():
     driver = get_test_driver()
     set_driver(driver)
     visit(start_page)
-    s("#header").should_have(exact_text("Selene"))
+    s("#header").should_have(exact_text(b"Selene"))
 
 
 def test_manual_start_2():
     visit(start_page)
-    s("#selene_link").should_have(exact_text("Selene site"))
+    s("#selene_link").should_have(exact_text(b"Selene site"))
 
 def test_auto_start():
     visit(start_page)
