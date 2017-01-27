957b28dd144beafe72475e525e8e73ca6c7dad0f
sepi <sepi@ciklum.com>
2017-01-27 13:14:06 +0200

    Gitter chat icon added to README

--- tests/integration/browser_factory_test.py
+++ tests/integration/browser_factory_test.py
@@ -1,7 +1,9 @@
 import pytest
 
+import selene
 from selene import factory
 from selene.browsers import Browser
+from selene.common.none_object import NoneObject
 from selene.tools import get_driver, set_driver
 from tests.acceptance.helpers.helper import get_test_driver
 
@@ -27,3 +29,31 @@ def test_can_set_browser_directly():
     assert get_driver().name == Browser.CHROME
     get_driver().quit()
     driver.quit()
+
+
+def test_is_driver_still_opened():
+    driver = get_test_driver()
+    assert factory.is_driver_still_open(driver)
+    driver.quit()
+    assert factory.is_driver_still_open(driver) is False
+
+
+def test_driver_has_started():
+    factory._start_driver("chrome")
+    assert factory.driver_has_started("chrome")
+    assert factory.driver_has_started("firefox") is False
+
+
+def test_ensure_driver_has_started():
+    driver = factory.ensure_driver_started("chrome")
+    assert driver.name == "chrome"
+
+
+def test_can_get_set_shared_driver():
+    selene.driver._shared_web_driver_source.driver = NoneObject("NoneObject")
+    shared_driver = factory.get_shared_driver()
+    assert isinstance(shared_driver, NoneObject)
+    factory.set_shared_driver(get_test_driver())
+    shared_driver = factory.get_shared_driver()
+    assert shared_driver.name == "firefox"
+    shared_driver.quit()
