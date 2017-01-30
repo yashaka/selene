02a69cd57e2dc675a940708cd32b2bad9c23004f
sepi <sepi@ciklum.com>
2017-01-30 13:37:32 +0200

    hold browser open config feature added

--- tests/integration/browser_factory_test.py
+++ tests/integration/browser_factory_test.py
@@ -1,11 +1,16 @@
+import os
+
 import pytest
 
 import selene
+from selene import config
 from selene import factory
 from selene.browsers import Browser
 from selene.common.none_object import NoneObject
-from selene.tools import get_driver, set_driver
+from selene.tools import get_driver, set_driver, visit
 from tests.acceptance.helpers.helper import get_test_driver
+
+start_page = 'file://' + os.path.abspath(os.path.dirname(__file__)) + '/../resources/start_page.html'
 
 
 @pytest.mark.parametrize("browser_name", ["firefox",
@@ -57,3 +62,20 @@ def test_can_get_set_shared_driver():
     shared_driver = factory.get_shared_driver()
     assert shared_driver.name == "firefox"
     shared_driver.quit()
+
+
+def test_can_hold_autocreated_browser_open():
+    config.hold_browser_open = True
+    visit(start_page)
+    driver = get_driver()
+    factory.kill_all_started_drivers()
+    assert factory.is_driver_still_open(driver)
+    driver.quit()
+
+
+def test_can_auto_close_browser():
+    config.hold_browser_open = False
+    visit(start_page)
+    driver = get_driver()
+    factory.kill_all_started_drivers()
+    assert factory.is_driver_still_open(driver) is False
