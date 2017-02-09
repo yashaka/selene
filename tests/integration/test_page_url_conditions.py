fd84da9899c8932b42adfdc2ee8f59eed2b66f74
sergey <sergey@gmail.com>
2017-02-09 21:00:03 +0200

    test for page condition added

--- tests/integration/test_page_url_conditions.py
+++ tests/integration/test_page_url_conditions.py
@@ -2,16 +2,16 @@ import os
 
 from selene import config
 from selene.conditions import exact_url
-from selene.tools import visit
+from selene.tools import visit, get_driver
 from selene.tools import wait_to
 
+start_page = 'file://' + os.path.abspath(os.path.dirname(__file__)) + '/../resources/start_page.html'
 
 
 def setup_module(m):
     config.browser_name = "chrome"
 
-def test_can_wait_for_exact_url():
-    start_page = 'file://' + os.path.abspath(os.path.dirname(__file__)) + '/../resources/start_page.html'
 
+def test_can_wait_for_exact_url():
     visit(start_page)
-    wait_to(exact_url(start_page))
+    wait_to(exact_url(get_driver().current_url))
