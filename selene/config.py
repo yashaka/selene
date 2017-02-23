171a77f542c7411466c591d045512b6436d77f90
Iakiv Kramarenko <yashaka@gmail.com>
2017-02-22 19:21:29 -0500

    #51: simplified implementation, renamed some sys variables names; deprecated config.app_host, now the config.base_url should be used

--- selene/config.py
+++ selene/config.py
@@ -1,4 +1,4 @@
-# todo: make the properties also "object oriented" to support different configs per different SeleneDriver instances
+# todo: make the properties also 'object oriented' to support different configs per different SeleneDriver instances
 import itertools
 import os
 import time
@@ -6,35 +6,31 @@ import time
 from selene.browsers import Browser
 from selene.helpers import env
 
-SELENE_TIMEOUT = "selene_timeout"
-SELENE_POOLING_INTERVAL = "selene_polling_interval"
-SELENE_BASE_URL = 'selene_base_url'
-SELENE_BROWSER = 'selene_browser'
-SELENE_START_MAXIMIZED = "selene_start_maximized"
-SELENE_HOLD_BROWSER_OPEN = "selene_hold_browser_open"
-SELENE_CACHE_ELEMENTS = "selene_cache_elements"
-SELENE_SCREENSHOT_FOLDER = "selene_screenshot_folder"
 
-timeout = env(SELENE_TIMEOUT) or 4
-poll_during_waits = env(SELENE_POOLING_INTERVAL) or 0.1
-app_host = env(SELENE_BASE_URL) or ''
+timeout = env('selene_timeout') or 4
+poll_during_waits = env('selene_poll_during_waits') or 0.1
+
+base_url = env('selene_base_url') or ''
+app_host = None
+# todo: we may probably refactor selene.config to selene.browser.config where config - is an object, not a module
+# todo: then it would be better to add warnings.warn("use base_url instead", DeprecationWarning)
 
 # todo: make cashing work (currently will not work...)
-cash_elements = env(SELENE_CACHE_ELEMENTS) == "True" or False
-"""To cash all elements after first successful find
-      config.cash_elements = True"""
+cash_elements = env('selene_cache_elements') == 'True' or False
+'''To cash all elements after first successful find
+      config.cash_elements = True'''
 
-browser_name = env(SELENE_BROWSER) or Browser.FIREFOX
+browser_name = env('selene_browser_name') or Browser.FIREFOX
 
-maximize_window = False if env(SELENE_START_MAXIMIZED) == "False" else True
+maximize_window = False if env('selene_maximize_window') == 'False' else True
 
-hold_browser_open = env(SELENE_HOLD_BROWSER_OPEN) == "True" or False
+hold_browser_open = env('selene_hold_browser_open') == 'True' or False
 
 counter = itertools.count(start=int(round(time.time() * 1000)))
 
-screenshot_folder = env(SELENE_SCREENSHOT_FOLDER) or os.path.join(os.path.expanduser("~"),
-                                                                  ".selene",
-                                                                  "screenshots",
-                                                                  str(next(counter)))
+screenshot_folder = env('selene_screenshot_folder') or os.path.join(os.path.expanduser('~'),
+                                                                    '.selene',
+                                                                    'screenshots',
+                                                                    str(next(counter)))
 
 desired_capabilities = None
