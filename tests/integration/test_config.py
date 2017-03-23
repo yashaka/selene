d3abe247bc5f06a4845d693f62fe4caeb8390898
sepi <sepi@ciklum.com>
2017-03-23 11:20:46 +0200

    test config foxed type error

--- tests/integration/test_config.py
+++ tests/integration/test_config.py
@@ -2,32 +2,32 @@ import os
 from selene import config
 from importlib import reload
 
-old_timeout = os.environ["selene_timeout"]
-old_polling_interval = os.environ["selene_poll_during_waits"]
-old_base_url = os.environ["selene_base_url"]
-old_browser_name = os.environ["selene_browser_name"]
-old_browser_maximize = os.environ["selene_maximize_window"]
-old_hold_browser_open = os.environ['selene_hold_browser_open']
+old_timeout = config.timeout
+old_polling_interval = config.poll_during_waits
+old_base_url = config.base_url
+old_browser_name = config.browser_name
+old_browser_maximize = config.maximize_window
+old_hold_browser_open = config.hold_browser_open
 
 
 def setup_module(m):
-    os.environ["selene_timeout"] = "3"
-    os.environ["selene_poll_during_waits"] = "0.5"
+    os.environ["selene_timeout"] = '3'
+    os.environ["selene_poll_during_waits"] = '0.5'
     os.environ["selene_base_url"] = "http://localhost"
     os.environ["selene_browser_name"] = "chrome"
-    os.environ["selene_maximize_window"] = "True"
-    os.environ['selene_hold_browser_open'] = "True"
+    os.environ["selene_maximize_window"] = 'True'
+    os.environ['selene_hold_browser_open'] = 'True'
 
     reload(config)
 
 
 def teardown_module(m):
-    os.environ["selene_timeout"] = old_timeout
-    os.environ["selene_poll_during_waits"] = old_polling_interval
+    os.environ["selene_timeout"] = str(old_timeout)
+    os.environ["selene_poll_during_waits"] = str(old_polling_interval)
     os.environ["selene_base_url"] = old_base_url
     os.environ["selene_browser_name"] = old_browser_name
-    os.environ["selene_maximize_window"] = old_browser_maximize
-    os.environ['selene_hold_browser_open'] = old_hold_browser_open
+    os.environ["selene_maximize_window"] = str(old_browser_maximize)
+    os.environ['selene_hold_browser_open'] = str(old_hold_browser_open)
 
 
 def test_timeout():
