c600a62ad5bb4ead1365f8f57cf0564b433cfa6f
yashaka <yashaka@gmail.com>
2020-01-06 14:30:18 +0200

    fixed test_opening_relative_url

--- tests/adhoc/test_shared_config_base_url.py
+++ tests/adhoc/test_shared_config_base_url.py
@@ -1,8 +1,8 @@
 from selene import browser, be, config
 
 
-def test_complete_task():
+def test_opening_relative_url():
     config.base_url = 'http://todomvc.com/examples/emberjs'
-    browser.open('/')
+    browser.open_url('/')
 
     browser.element('#new-todo').should(be.visible)
