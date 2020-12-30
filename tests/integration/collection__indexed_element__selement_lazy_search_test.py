34b4b2435778f4bb6f6810636ed24d335ed032da
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2020-12-30 10:19:41 +0300

    #203 resurrected indexed_selement_lazy_search_test

--- tests/integration/collection__indexed_element__selement_lazy_search_test.py
+++ tests/integration/collection__indexed_element__selement_lazy_search_test.py
@@ -19,71 +19,51 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-from selene.api.past import config
-from selene.common.none_object import NoneObject
-from selene.api.past import SeleneDriver
-from tests_from_past.past.acceptance import get_test_driver
-from tests_from_past.integration.helpers import GivenPage
-
-__author__ = 'yashaka'
-
-driver = NoneObject('driver')  # type: SeleneDriver
-GIVEN_PAGE = NoneObject('GivenPage')  # type: GivenPage
-WHEN = GIVEN_PAGE  # type: GivenPage
-original_timeout = config.timeout
+from tests.integration.helpers.givenpage import GivenPage
 
 
-def setup_module(m):
-    global driver
-    driver = SeleneDriver.wrap(get_test_driver())
-    global GIVEN_PAGE
-    GIVEN_PAGE = GivenPage(driver)
-    global WHEN
-    WHEN = GIVEN_PAGE
+def test_search_is_lazy_and_does_not_start_on_creation_for_both_collection_and_indexed(session_browser):
+    page = GivenPage(session_browser)
+    page.opened_empty()
 
+    non_existent_element = session_browser.all('.will-appear')[1]
 
-def teardown_module(m):
-    driver.quit()
-
-
-def setup_function(fn):
-    global original_timeout
-    config.timeout = original_timeout
-
-
-def test_search_is_lazy_and_does_not_start_on_creation_for_both_collection_and_indexed():
-    GIVEN_PAGE.opened_empty()
-    non_existent_element = driver.all('.will-appear')[1]
     assert str(non_existent_element)
 
 
-def test_search_is_postponed_until_actual_action_like_questioning_displayed():
-    GIVEN_PAGE.opened_empty()
-    element = driver.all('.will-appear')[1]
+def test_search_is_postponed_until_actual_action_like_questioning_displayed(session_browser):
+    page = GivenPage(session_browser)
+    page.opened_empty()
 
-    WHEN.load_body('''
+    element = session_browser.all('.will-appear')[1]
+
+    page.load_body('''
                    <ul>Hello to:
                        <li class="will-appear">Bob</li>
                        <li class="will-appear">Kate</li>
                    </ul>''')
+
     assert element.is_displayed() is True
 
 
-def test_search_is_updated_on_next_actual_action_like_questioning_displayed():
-    GIVEN_PAGE.opened_empty()
-    element = driver.all('.will-appear')[1]
+def test_search_is_updated_on_next_actual_action_like_questioning_displayed(session_browser):
+    page = GivenPage(session_browser)
+    page.opened_empty()
 
-    WHEN.load_body('''
+    element = session_browser.all('.will-appear')[1]
+
+    page.load_body('''
                    <ul>Hello to:
                        <li class="will-appear">Bob</li>
                        <li class="will-appear">Kate</li>
                    </ul>''')
+
     assert element.is_displayed() is True
 
-    WHEN.load_body('''
+    page.load_body('''
                    <ul>Hello to:
                        <li class="will-appear">Bob</li>
                        <li class="will-appear" style="display:none">Kate</li>
                    </ul>''')
+
     assert element.is_displayed() is False
