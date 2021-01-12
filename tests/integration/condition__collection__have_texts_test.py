099ad3936c53290a65ee2469433ddc43a1be73e0
Iakiv Kramarenko <yashaka@gmail.com>
2021-01-12 22:29:56 +0200

    regroupped tests for easier coverage tracking – noticed that should_have_text was absent and added it

--- tests/integration/condition__collection__have_texts_test.py
+++ tests/integration/condition__collection__have_texts_test.py
@@ -46,6 +46,22 @@ def test_should_have_texts(session_browser):
     session_browser.all('li').should(have.texts('Alex!', 'Yakov!'))
 
 
+def test_should_have_texts_exception(session_browser):
+    browser = session_browser.with_(timeout=0.1)
+    GivenPage(browser.driver).opened_with_body(
+        '''
+        <ul>Hello:
+           <li>Alex</li>
+           <li>Yakov</li>
+        </ul>
+        ''')
+
+    with pytest.raises(TimeoutException) as error:
+        browser.all('li').should(have.texts('Alex'))
+    assert "has texts ('Alex',)" in error.value.msg
+    assert "AssertionError: actual visible_texts: ['Alex', 'Yakov']" in error.value.msg
+
+
 def test_should_have_no_texts(session_browser):
     GivenPage(session_browser.driver).opened_with_body(
         '''
@@ -62,67 +78,7 @@ def test_should_have_no_texts(session_browser):
     session_browser.all('li').should(have.no.texts('Alex!'))
 
 
-def test_should_have_no_text(session_browser):
-    GivenPage(session_browser.driver).opened_with_body(
-        '''
-        <ul>Hello:
-           <li>Yakov</li>
-           <li>Yakov</li>
-        </ul>
-        ''')
-
-    session_browser.all('li').should(have.no.text('L'))
-
-
-def test_have_texts_exception(session_browser):
-    browser = session_browser.with_(timeout=0.1)
-    GivenPage(browser.driver).opened_with_body(
-        '''
-        <ul>Hello:
-           <li>Alex</li>
-           <li>Yakov</li>
-        </ul>
-        ''')
-
-    with pytest.raises(TimeoutException) as error:
-        browser.all('li').should(have.texts('Alex'))
-    assert "has texts ('Alex',)" in error.value.msg
-    assert "AssertionError: actual visible_texts: ['Alex', 'Yakov']" in error.value.msg
-
-
-def test_have_text_exception(session_browser):
-    browser = session_browser.with_(timeout=0.1)
-    GivenPage(browser.driver).opened_with_body(
-        '''
-        <ul>Hello:
-           <li>Alex</li>
-           <li>Alex</li>
-        </ul>
-        ''')
-
-    with pytest.raises(TimeoutException) as error:
-        browser.all('li').should(have.text('Yakov'))
-    assert "has text Yakov" in error.value.msg
-    assert "AssertionError: actual text: Alex" in error.value.msg
-
-
-def test_have_no_text_exception(session_browser):
-    browser = session_browser.with_(timeout=0.1)
-    GivenPage(browser.driver).opened_with_body(
-        '''
-        <ul>Hello:
-           <li>Alex</li>
-           <li>Alex</li>
-        </ul>
-        ''')
-
-    with pytest.raises(TimeoutException) as error:
-        browser.all('li').should(have.no.text('Alex'))
-    assert "has no text Alex" in error.value.msg
-    assert "ConditionNotMatchedError: condition not matched" in error.value.msg
-
-
-def test_have_no_texts_exception(session_browser):
+def test_should_have_no_texts_exception(session_browser):
     browser = session_browser.with_(timeout=0.1)
     GivenPage(browser.driver).opened_with_body(
         '''
@@ -138,3 +94,60 @@ def test_have_no_texts_exception(session_browser):
     assert "has no texts ('Alex', 'Yakov')" in error.value.msg
     assert "ConditionNotMatchedError: condition not matched" in error.value.msg
     # todo: should not we see here actual texts in log too?
+
+
+def test_should_have_text(session_browser):
+    GivenPage(session_browser.driver).opened_with_body(
+        '''
+        <ul>Hello:
+           <li>Yakov</li>
+           <li>Jakob</li>
+        </ul>
+        ''')
+
+    session_browser.all('li').should(have.text('ako'))
+    session_browser.all('li').should(have.text(''))
+
+
+def test_should_have_text_exception(session_browser):
+    browser = session_browser.with_(timeout=0.1)
+    GivenPage(browser.driver).opened_with_body(
+        '''
+        <ul>Hello:
+           <li>Alex</li>
+           <li>Alex</li>
+        </ul>
+        ''')
+
+    with pytest.raises(TimeoutException) as error:
+        browser.all('li').should(have.text('Yakov'))
+    assert "has text Yakov" in error.value.msg
+    assert "AssertionError: actual text: Alex" in error.value.msg
+
+
+def test_should_have_no_text(session_browser):
+    GivenPage(session_browser.driver).opened_with_body(
+        '''
+        <ul>Hello:
+           <li>Yakov</li>
+           <li>Yakov</li>
+        </ul>
+        ''')
+
+    session_browser.all('li').should(have.no.text('L'))
+
+
+def test_should_have_no_text_exception(session_browser):
+    browser = session_browser.with_(timeout=0.1)
+    GivenPage(browser.driver).opened_with_body(
+        '''
+        <ul>Hello:
+           <li>Alex</li>
+           <li>Alex</li>
+        </ul>
+        ''')
+
+    with pytest.raises(TimeoutException) as error:
+        browser.all('li').should(have.no.text('Alex'))
+    assert "has no text Alex" in error.value.msg
+    assert "ConditionNotMatchedError: condition not matched" in error.value.msg
