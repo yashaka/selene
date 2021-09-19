56a73170a6346c3d9094f360358769cfaabcaaca
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2021-09-19 16:45:14 +0300

    [#203] port test_bys to selene v2

--- tests/bys_test.py
+++ tests/bys_test.py
@@ -19,69 +19,58 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+from selene import by
 
-from selene.bys import (
-    by,
-    by_css,
-    by_name,
-    by_link_text,
-    by_partial_link_text,
-    by_xpath,
-    following_sibling,
-    parent,
-    first_child,
-    by_text,
-    by_partial_text,
-    escape_text_quotes_for_xpath,
-)
+
+def test_by_id():
+    assert by.id('c') == ('id', 'c')
 
 
 def test_by_css():
-    assert by("a") == ('css selector', 'a')
-    assert by_css("span") == ('css selector', 'span')
+    assert by.css("a") == ('css selector', 'a')
 
 
 def test_by_name():
-    assert by_name("test") == ('name', 'test')
+    assert by.name("test") == ('name', 'test')
 
 
 def test_by_link_text():
-    assert by_link_text("text") == ('link text', 'text')
+    assert by.link_text("text") == ('link text', 'text')
 
 
 def test_by_partial_link_text():
-    assert by_partial_link_text("text") == ("partial link text", "text")
+    assert by.partial_link_text("text") == ("partial link text", "text")
 
 
 def test_by_xpath():
-    assert by_xpath("//a") == ('xpath', "//a")
+    assert by.xpath("//a") == ('xpath', "//a")
 
 
 def test_by_following_sibling():
-    assert following_sibling() == ("xpath", './following-sibling::*')
+    assert by.be_following_sibling() == ("xpath", './following-sibling::*')
 
 
 def test_by_parent():
-    assert parent() == ("xpath", "..")
+    assert by.be_parent() == ("xpath", "..")
 
 
 def test_first_child():
-    assert first_child() == ("xpath", "./*[1]")
+    assert by.be_first_child() == ("xpath", "./*[1]")
 
 
 def test_by_text():
-    assert by_text("test") == (
+    assert by.text("test") == (
         "xpath",
         './/*[text()[normalize-space(.) = concat("", "test")]]',
     )
 
 
 def test_by_partial_text():
-    assert by_partial_text("text") == (
+    assert by.partial_text("text") == (
         "xpath",
         './/*[text()[contains(normalize-space(.), concat("", "text"))]]',
     )
 
 
 def test_by_escape_text_quotes_for_xpath():
-    assert escape_text_quotes_for_xpath('test') == 'concat("", "test")'
+    assert by._escape_text_quotes_for_xpath('test') == 'concat("", "test")'
