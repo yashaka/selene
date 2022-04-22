6dbd3170e72457b22cc8aedfe0fd0b91b6796d0e
yashaka <yashaka@gmail.com>
2022-04-22 15:14:13 +0300

    CHORE: update copyright (2022)

--- tests/resources/orderapp/app.js
+++ tests/resources/orderapp/app.js
@@ -1,7 +1,7 @@
 /*
 # MIT License
 #
-# Copyright (c) 2015-2021 Iakiv Kramarenko
+# Copyright (c) 2015-2022 Iakiv Kramarenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
