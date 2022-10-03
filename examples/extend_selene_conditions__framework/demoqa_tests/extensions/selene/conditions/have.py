7107c0adb1210020e70fd8376649804184921d3a
yashaka <yashaka@gmail.com>
2022-10-03 18:05:00 +0300

    DOCS: tune «extending selene have.* conditions» example

--- examples/extend_selene_conditions__framework/demoqa_tests/extensions/selene/conditions/have.py
+++ examples/extend_selene_conditions__framework/demoqa_tests/extensions/selene/conditions/have.py
@@ -1,7 +1,61 @@
 import datetime
+import sys
 
 from selene import have
 from selene.support.conditions.have import *  # noqa
+
+
+def date(value: datetime.date):  # noqa
+    """
+    match date according to the project specific format
+    """
+    month_full_name = '%B'
+    year_full = '%Y'
+
+    is_unix_like = sys.platform in ('linux', 'linux2', 'darwin')
+    day_without_leading_zero = '%-d' if is_unix_like else '%#d'
+    hours_without_leading_zero = '%-I' if is_unix_like else '%#I'
+
+    minutes_00_59 = '%M'
+    am_or_pm = '%p'
+
+    return have.value(
+        value.strftime(
+            f'{month_full_name} '
+            f'{day_without_leading_zero}, '
+            f'{year_full} '
+            f'{hours_without_leading_zero}:'
+            f'{minutes_00_59} '
+            f'{am_or_pm}'
+        )
+    )
+
+
+'''
+# Just another versions of implementation...
+
+
+def date(value: datetime.date):  # noqa
+    """
+    match date according to the project specific format
+    """
+    month_full_name = '%B'
+    year_full = '%Y'
+    day_01_31 = '%d'
+    hours_00_59 = '%I'
+    minutes_00_59 = '%M'
+    am_or_pm = '%p'
+
+    return have.value(
+        value.strftime(
+            f'{month_full_name} '
+            f'{day_01_31}, '
+            f'{year_full} '
+            f'{hours_00_59}:'
+            f'{minutes_00_59} '
+            f'{am_or_pm}'
+        ).replace(' 0', ' ')
+    )
 
 
 def date(value: datetime.date):  # noqa
@@ -25,3 +79,4 @@ def date(value: datetime.date):  # noqa
             f'{am_or_pm}'
         )
     )
+'''
