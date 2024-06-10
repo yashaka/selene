e406272608f86c16028129ee2015331789f83a50
yashaka <yashaka@gmail.com>
2024-06-10 21:52:38 +0300

    CHORE: update pylint and some deps

--- .run/lint_pylint.sh
+++ .run/lint_pylint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touch __init__.py
-pylint $(pwd) --disable="$(cat .pylint-disabled-rules)" --ignore-patterns=.venv -r n
+pylint $(pwd) --disable="$(cat .pylint-disabled-rules)" --ignore-paths=\.venv --ignore-patterns=.*\.pyi -r n
 rm __init__.py
