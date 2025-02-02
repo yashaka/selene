fb891caed0e3ac3a405d52f416a7c3fe0796db2d
yashaka <yashaka@gmail.com>
2025-02-02 14:35:52 +0200

    STYLE: fix styles for query unit tests

--- tests/unit/common/_typing_fucntions__query_test.py
+++ tests/unit/common/_typing_fucntions__query_test.py
@@ -12,8 +12,8 @@ def test_query_name_and_application():
     is_positive = Query('is positive', lambda x: x > 0)
 
     assert 'is positive' == str(is_positive)
-    assert is_positive(1) == True
-    assert is_positive(0) == False
+    assert is_positive(1) is True
+    assert is_positive(0) is False
 
 
 def test_query_recomposition():
@@ -24,14 +24,14 @@ def test_query_recomposition():
     )
 
     assert 'is positive incremented by 0' == str(is_positive_increented_by(0))
-    assert is_positive_increented_by(0)(0) == False
-    assert is_positive_increented_by(1)(0) == True
-    assert is_positive_increented_by(1)(-1) == False
-    assert is_positive_increented_by(2)(-1) == True
+    assert is_positive_increented_by(0)(0) is False
+    assert is_positive_increented_by(1)(0) is True
+    assert is_positive_increented_by(1)(-1) is False
+    assert is_positive_increented_by(2)(-1) is True
 
     # TODO: consider the is_positive_increented_by(1).as('is increment positive') syntax
     is_increment_positive = Query('is increment positive', is_positive_increented_by(1))
 
     assert 'is increment positive' == str(is_increment_positive)
-    assert is_increment_positive(0) == True
-    assert is_increment_positive(-1) == False
+    assert is_increment_positive(0) is True
+    assert is_increment_positive(-1) is False
