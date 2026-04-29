784e7ff7f67d9b4d46f364787c141807ab7396b3
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2026-04-30 00:07:50 +0300

    TEST: enable timeout coverage for core wait visibility

--- tests/integration/core_wait_test.py
+++ tests/integration/core_wait_test.py
@@ -1,8 +1,10 @@
 import time
 
+import pytest
 from selenium.webdriver.common.by import By
 
 from selene import be
+from selene.core.exceptions import TimeoutException
 from tests.integration.helpers.givenpage import GivenPage
 
 
@@ -27,10 +29,9 @@ def test_waits_for_visibility_minimum_needed_time(session_browser):
     assert "second" in session_browser.driver.current_url
 
 
-def x_test_fails_on_timeout_during_waits_first_for_present_in_dom_then_visibility(
+def test_fails_on_timeout_during_waiting_for_visibility(
     session_browser,
 ):
-    # TODO: fix the test, it does not reflect its test goal reflected in summary
     page = GivenPage(session_browser.driver)
     page.opened_with_body(
         '''
@@ -40,12 +41,5 @@ def x_test_fails_on_timeout_during_waits_first_for_present_in_dom_then_visibilit
     ).execute_script_with_timeout(
         'document.getElementsByTagName("a")[0].style = "display:block";', 1.1
     )
-    stamp_before = time.time_ns()
-    session_browser.element('a').wait.at_most(1).for_(be.visible)
-
-    session_browser.driver.find_element(By.CSS_SELECTOR, 'a').click()
-
-    stamp_after = time.time_ns()
-    deviation_sec = 0.2
-    assert stamp_after - stamp_before < (1 + deviation_sec) * pow(10, 9)
-    assert "second" in session_browser.driver.current_url
+    with pytest.raises(TimeoutException):
+        session_browser.element('a').wait.at_most(1).for_(be.visible)
