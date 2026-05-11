2ed0cd71fc77b13f6dceecc57f63cec63b0fddd3
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2026-05-11 23:13:20 +0300

    FIX #433 Collection: handle or_ fallback and preserve condition names

--- tests/integration/collection__should__keeps_condition_name_test.py
+++ tests/integration/collection__should__keeps_condition_name_test.py
@@ -26,32 +26,3 @@ def test_collection_should_keeps_entity_aware_condition_name(session_browser):
 
         assert "browser.all(('css selector', 'li')).have size 2" in message
         assert "browser.all(('css selector', 'li')).has size 2" not in message
-
-
-def test_collection_should_keeps_ignore_case_condition_name(session_browser):
-    browser = session_browser.with_(timeout=0.1)
-
-    GivenPage(browser.driver).opened_with_body('''
-        <ul>
-            <li>1) One!!!</li>
-            <li>2) Two...</li>
-            <li>3) Three???</li>
-        </ul>
-        ''')
-
-    try:
-        browser.all('li').should(have.texts('one.', 'two.', 'three.').ignoring_case)
-
-        pytest.fail('should have failed on collection texts mismatch')
-
-    except TimeoutException as error:
-        message = str(error)
-
-        assert (
-            "browser.all(('css selector', 'li')).have texts ignoring case: "
-            "['one.', 'two.', 'three.']"
-        ) in message
-        assert (
-            "browser.all(('css selector', 'li')).have texts "
-            "['one.', 'two.', 'three.']"
-        ) not in message
