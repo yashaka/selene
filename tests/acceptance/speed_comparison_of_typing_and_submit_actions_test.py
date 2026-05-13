ae08a1c73802ef9848b42d5f3a9c96c5ac7cb3ff
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2026-05-13 13:26:35 +0300

    TESTS #62 (speed): move benchmarks to local fixtures, add flow benchmark, tighten cached ratio to 1.10 (#653)

--- tests/acceptance/speed_comparison_of_typing_and_submit_actions_test.py
+++ tests/acceptance/speed_comparison_of_typing_and_submit_actions_test.py
@@ -1,32 +1,12 @@
 # MIT License
-#
-# Copyright (c) 2015-2022 Iakiv Kramarenko
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import pytest
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 from selene import be
-
 from selene.support.shared import browser
 from tests import resources
 from tests.acceptance.helpers.helper import get_test_driver
@@ -34,11 +14,11 @@ from tests.helpers import repeated_time_spent
 
 pytestmark = [pytest.mark.speed]
 
-BENCHMARK_INPUT_URL = resources.url('speed_benchmark_input.html')
-# 2 warmups reduce cold-start noise; 9 repeats keep median stable
-# while test runtime stays acceptable for CI and local runs.
+BENCHMARK_FLOW_URL = resources.url('speed_benchmark_flow.html')
+# Keep aligned with the isolated benchmark to compare ratios consistently.
 BENCHMARK_REPEATS = 9
 BENCHMARK_WARMUPS = 2
+TASKS = 10
 
 selenium_browser: WebDriver
 
@@ -46,7 +26,7 @@ selenium_browser: WebDriver
 def setup_function():
     global selenium_browser
     selenium_browser = get_test_driver()
-    selenium_browser.get(BENCHMARK_INPUT_URL)
+    selenium_browser.get(BENCHMARK_FLOW_URL)
     WebDriverWait(selenium_browser, 4).until(
         expected_conditions.visibility_of_element_located(
             (By.CSS_SELECTOR, "#new-todo")
@@ -54,7 +34,7 @@ def setup_function():
     )
 
     browser.config.driver = get_test_driver()
-    browser.open(BENCHMARK_INPUT_URL)
+    browser.open(BENCHMARK_FLOW_URL)
     browser.element("#new-todo").should(be.visible)
 
 
@@ -65,33 +45,41 @@ def teardown_function():
 
 
 def create_tasks_with_raw_selenium():
-    global selenium_browser
     new_todo = selenium_browser.find_element(By.CSS_SELECTOR, "#new-todo")
-    for task_text in map(str, range(10)):
-        new_todo.clear()
-        new_todo.send_keys(task_text)
+    for task_text in map(str, range(TASKS)):
+        new_todo.send_keys(task_text + Keys.ENTER)
 
 
 def create_tasks_with_selenium_with_research():
-    global selenium_browser
-    for task_text in map(str, range(10)):
+    for task_text in map(str, range(TASKS)):
         new_todo = selenium_browser.find_element(By.CSS_SELECTOR, "#new-todo")
-        new_todo.clear()
-        new_todo.send_keys(task_text)
+        new_todo.send_keys(task_text + Keys.ENTER)
 
 
 def create_tasks_with_selene_and_send_keys():
-    for task_text in map(str, range(10)):
-        browser.element("#new-todo").clear().send_keys(task_text)
+    for task_text in map(str, range(TASKS)):
+        browser.element("#new-todo").send_keys(task_text + Keys.ENTER)
 
 
 def create_tasks_with_selene_with_cache():
     new_todo = browser.element("#new-todo").cached
-    for task_text in map(str, range(10)):
-        new_todo.clear().send_keys(task_text)
+    for task_text in map(str, range(TASKS)):
+        new_todo.send_keys(task_text + Keys.ENTER)
 
 
-# TODO: review these tests
+def _print_result(selene_time, selenium_time, selene_samples, selenium_samples):
+    ratio = selene_time / selenium_time
+    print(
+        f"median {selene_time} vs {selenium_time}; ratio={ratio}; "
+        f"selene={selene_samples}; selenium={selenium_samples}"
+    )
+
+
+def _assert_submitted_items_count():
+    assert (
+        len(selenium_browser.find_elements(By.CSS_SELECTOR, "#todo-list li")) >= TASKS
+    )
+    assert len(browser.all("#todo-list li").locate()) >= TASKS
 
 
 def test_selene_is_almost_as_fast_selenium_with_research_and_initial_wait_for_visibility():
@@ -105,11 +93,8 @@ def test_selene_is_almost_as_fast_selenium_with_research_and_initial_wait_for_vi
         repeats=BENCHMARK_REPEATS,
         warmups=BENCHMARK_WARMUPS,
     )
-    ratio = selene_time / selenium_time
-    print(
-        f"median {selene_time} vs {selenium_time}; ratio={ratio}; "
-        f"selene={selene_samples}; selenium={selenium_samples}"
-    )
+    _print_result(selene_time, selenium_time, selene_samples, selenium_samples)
+    _assert_submitted_items_count()
     assert selene_time < 1.25 * selenium_time
 
 
@@ -125,11 +110,8 @@ def test_selene_is_from_32_to_75_percents_slower_than_raw_selenium():
         repeats=BENCHMARK_REPEATS,
         warmups=BENCHMARK_WARMUPS,
     )
-    ratio = selene_time / selenium_time
-    print(
-        f"median {selene_time} vs {selenium_time}; ratio={ratio}; "
-        f"selene={selene_samples}; selenium={selenium_samples}"
-    )
+    _print_result(selene_time, selenium_time, selene_samples, selenium_samples)
+    _assert_submitted_items_count()
     assert selene_time <= 1.75 * selenium_time
 
 
@@ -144,9 +126,6 @@ def test_cached_selene_is_almost_as_fast_raw_selenium():
         repeats=BENCHMARK_REPEATS,
         warmups=BENCHMARK_WARMUPS,
     )
-    ratio = selene_time / selenium_time
-    print(
-        f"median {selene_time} vs {selenium_time}; ratio={ratio}; "
-        f"selene={selene_samples}; selenium={selenium_samples}"
-    )
+    _print_result(selene_time, selenium_time, selene_samples, selenium_samples)
+    _assert_submitted_items_count()
     assert selene_time < 1.10 * selenium_time
