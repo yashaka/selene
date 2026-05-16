626c4e63301401001baac6508c3b022cc2af2f80
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2026-05-16 20:00:58 +0300

    black

--- tests/integration/element__perform__js__drop_file_test.py
+++ tests/integration/element__perform__js__drop_file_test.py
@@ -10,25 +10,23 @@ def test_drops_file_to_self_when_input_type_file_inside_target(session_browser):
     browser = session_browser
     page = GivenPage(browser.driver)
     page.opened_empty()
-    page.load_body(
-        """
+    page.load_body("""
         <div id="dropzone">
           <input id="file-input" type="file" style="display:none" />
           <span id="result"></span>
         </div>
-        """
-    )
-    page.execute_script(
-        """
+        """)
+    page.execute_script("""
         document.getElementById('file-input').addEventListener('change', function () {
           var value = this.value || '';
           var fileName = value.split('\\\\').pop();
           document.getElementById('result').textContent = fileName;
         });
-        """
-    )
+        """)
 
-    browser.element('#dropzone').perform(command.js.drop_file(resources.path('selenite.png')))
+    browser.element('#dropzone').perform(
+        command.js.drop_file(resources.path('selenite.png'))
+    )
 
     browser.element('#result').should(have.exact_text('selenite.png'))
 
@@ -36,23 +34,19 @@ def test_drops_file_to_self_when_input_type_file_inside_target(session_browser):
 def test_element_drop_file_shortcut(session_browser):
     browser = session_browser
     page = GivenPage(browser.driver)
-    page.opened_with_body(
-        """
+    page.opened_with_body("""
         <div id="dropzone">
           <input id="file-input" type="file" style="display:none" />
           <span id="result"></span>
         </div>
-        """
-    )
-    page.execute_script(
-        """
+        """)
+    page.execute_script("""
         document.getElementById('file-input').addEventListener('change', function () {
           var value = this.value || '';
           var fileName = value.split('\\\\').pop();
           document.getElementById('result').textContent = fileName;
         });
-        """
-    )
+        """)
 
     browser.element('#dropzone').drop_file(resources.path('selenite.png'))
 
